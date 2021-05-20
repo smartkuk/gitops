b2b3fb23882e0d05b392942f69b10fbd3f95139e
jeongkuk.kim <jeoungkuk.kim@bankwareglobal.com>
2021-05-20 14:39:32 +0900

    update

--- operations/send_istio.sh
+++ operations/send_istio.sh
@@ -12,4 +12,15 @@ echo "send request istio(with http header) => http://${minikube_ip}:${http_port}
 blue=$(curl -H "bxcr-user: smartkuk" http://${minikube_ip}:${http_port}/default/spring-boot-rest/app/name)
 echo ">>> ${blue}"
 echo ""
+
+echo "send request in container => http://spring-boot-rest-blue.default.svc.cluster.local/app/name"
+res=$(kubectl exec -it nginx -c nginx -- curl http://spring-boot-rest-blue.default.svc.cluster.local/app/name)
+echo ">>> ${res}"
+echo ""
+
+echo "send request in container(with http header) => http://spring-boot-rest-green.default.svc.cluster.local/app/name"
+res=$(kubectl exec -it nginx -c nginx -- curl -H "bxcr-user: smartkuk" http://spring-boot-rest-green.default.svc.cluster.local/app/name)
+echo ">>> ${res}"
+echo ""
+
 date
